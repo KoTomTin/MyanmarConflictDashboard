5b733a0445d08eb15be6114140d90e95a9466853
KaungTin <kaung.tin@city.ac.uk>
2025-10-01 10:33:13 +0100

    Update: replace repo contents with new code

--- app.py
+++ app.py
@@ -62,4 +62,4 @@ app.layout = dbc.Container(
 
 if __name__ == "__main__":
     # Explicitly enable reloader and hot reload to ensure page edits reflect immediately
-    app.run(debug=True, port=8050, use_reloader=True, dev_tools_hot_reload=True)
+    app.run(debug=True, port=8050)
